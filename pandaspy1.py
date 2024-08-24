d577c80eacddb67d234008ce7f4380e8eaf5c189
ptanshul <anshul.tiwari@rediffmail.com>
2024-08-24 23:22:23 +0530

    commit 1

---
+++ pandaspy1.py
@@ -0,0 +1,8 @@
+data = {
+    'Name': ['Alice', 'Bob', 'Charlie', 'David'],
+    'Age': [24, 27, 22, 32],
+    'City': ['New York', 'Los Angeles', 'Chicago', 'Houston']
+}
+
+df = pd.DataFrame(data)
+print(df)
