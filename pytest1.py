80e9f79e0032a78a6e83d718a298daea495669fa
ptanshul <anshul.tiwari@rediffmail.com>
2024-08-24 21:19:41 +0530

    comit with result

--- pytest1.py
+++ pytest1.py
@@ -10,4 +10,15 @@ def test_add():
 def test_add_negative():
     assert add(-7, -1) == -8
     
+#
+#====================================================== 2 passed in 0.04s ======================================================= 
+#PS H:\python\python-hyd\pythonlbs> python -m pytest pytest1.py
+#===================================================== test session starts ======================================================
+#platform win32 -- Python 3.11.9, pytest-8.3.2, pluggy-1.5.0
+#rootdir: H:\python\python-hyd\pythonlbs
+#collected 2 items                                                                                                                
+
+#pytest1.py ..                                                                                                             [100%] 
+
+#====================================================== 2 passed in 0.03s ======================================================= 
 
