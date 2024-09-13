8a735ef460795f2a3fafeaab39c101bb3d947927
ptanshul <anshul.tiwari@rediffmail.com>
2024-09-14 00:58:05 +0530

    cc2

--- progressbar.py
+++ progressbar.py
@@ -1,7 +1,7 @@
 from rich.progress import progress
 import time
 
-with Progress() as progress:
+with progress() as progress:
     task = progress.add_task("[cyan]Downloading ", total=100)
     while not progress.finished:
         progress.update(task, advance=1)
