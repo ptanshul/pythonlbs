e2cdc0ebeaaf17fffafc6d2c4280b70942bf14a7
ptanshul <anshul.tiwari@rediffmail.com>
2024-10-15 14:10:48 +0530

    commit6

--- generator2.py
+++ generator2.py
@@ -1,9 +1,4 @@
-def squares(n):
-  """Returns a list of squares from 0 to n-1."""
-  result = []
-  for i in range(n):
-    result.append(i * i)
-  return result
+
 
 # Generator function
 def squares1(n):
@@ -11,9 +6,7 @@ def squares1(n):
   for i in range(n):
     yield i * i * i
 
-# Using the functions
-num_list = squares(5)  # [0, 1, 4, 9, 16]
-print(num_list)
+
 
 num_list1 = squares1(5)  # [0, 1, 4, 9, 16]
 print(num_list1)
