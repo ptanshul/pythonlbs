d6d8f3eb63ed888718a8ada38078e58de4e5dfe4
ptanshul <anshul.tiwari@rediffmail.com>
2024-10-15 11:46:59 +0530

    commit2

--- decorator1.py
+++ decorator1.py
@@ -1,1 +1,13 @@
 print("script for decorators")
+
+def decorator_function(fun):
+    def wrapper_function():
+        print("wash hands and wash face ")
+        fun()
+        print("Bartan dona bartan dona")
+    return wrapper_function
+
+@decorator_function
+def khana_banana():
+    print("Cook food Cook Nice food !!!!!")
+khana_banana()
