36cfed5e116bed8894a6c9ece2e37de97fc47276
ptanshul <anshul.tiwari@rediffmail.com>
2024-10-15 13:31:48 +0530

    comimit3

--- generator2.py
+++ generator2.py
@@ -9,7 +9,7 @@ def squares(n):
 def squares1(n):
   """Generates squares from 0 to n-1."""
   for i in range(n):
-    yield i * i
+    yield i * i * i
 
 # Using the functions
 num_list = squares(5)  # [0, 1, 4, 9, 16]
